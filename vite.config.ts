7e4ff705bd097ac4ae8f3fd89fbc4a1068ca1cd1
Denys Pavlykivskyi <denyspavlykivskyi.dev@outlook.com>
2025-05-23 23:11:32 -0800

    updated commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Json2PDF/',
+  base: '/Json2PdfConverter/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],

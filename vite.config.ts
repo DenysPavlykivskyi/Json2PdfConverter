193c18c6ba69964ff59155974c330dac4f2d13a9
Denys Pavlykivskyi <denyspavlykivskyi.dev@outlook.com>
2025-05-23 23:30:06 -0800

    updated commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/Json2PdfConverter/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
